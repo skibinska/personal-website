5ea642c3c001e3ff71f1e83dff88cd1968dd7226
ewelina <skibinska.ewelina@gmail.com>
2017-01-22 00:48:30 +0000

    Add animation to navigation

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,8 +1,16 @@
 (function () {
   'use strict';
   var navIcon = document.querySelector('.nav--icon');
+  var navigation = document.querySelector('.navigation');
   var toggleClass = function () {
-    this.classList.toggle('open');
+    this.classList.toggle('active');
+    if (navigation.classList.contains('active')) {
+      navigation.classList.remove('active', 'slideDown');
+      navigation.classList.add('slideUp');
+    } else {
+      navigation.classList.remove('slideUp');
+      navigation.classList.add('active', 'slideDown');
+    }
   }.bind(navIcon);
   navIcon.addEventListener('click', toggleClass);
 })();
