f22e178c6b0dd6a50f58d73447f09a2439d26f8d
ewelina <skibinska.ewelina@gmail.com>
2017-01-27 21:57:36 +0000

    Add fixed header

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,8 +1,9 @@
 (function () {
   'use strict';
   var navIcon = document.querySelector('.nav--icon');
-  var navigation = document.querySelector('.navigation');
+  var header = document.querySelector('.header');
   var toggleClass = function () {
+    var navigation = document.querySelector('.navigation');
     this.classList.toggle('active');
     if (navigation.classList.contains('active')) {
       navigation.classList.remove('active', 'js-slide-down');
@@ -12,5 +13,18 @@
       navigation.classList.add('active', 'js-slide-down');
     }
   }.bind(navIcon);
+  var toggleFixedHeader = function () {
+    var parallax = document.querySelector('.parallax');
+    var parallaxHeight = parallax.offsetHeight;
+    if (document.body.scrollTop >= parallaxHeight) {
+      console.log(document.body.scrollTop, ' scrollTop');
+      header.classList.remove('header--default', 'js-slide-up');
+      header.classList.add('js-header--fixed', 'js-slide-down');
+    } else {
+      header.classList.remove('js-header--fixed', 'js-slide-down');
+      header.classList.add('header--default', 'js-slide-up');
+    }
+  };
   navIcon.addEventListener('click', toggleClass);
+  window.addEventListener('scroll', toggleFixedHeader);
 })();
