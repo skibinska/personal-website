76e279b69a270142ea74927cc8f5ce812cea5557
ewelina <skibinska.ewelina@gmail.com>
2017-01-29 11:27:59 +0000

    Fix header on mobile

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -4,24 +4,32 @@
   var header = document.querySelector('.header');
   var toggleClass = function () {
     var navigation = document.querySelector('.navigation');
+    var isOpen = false;
     this.classList.toggle('active');
     if (navigation.classList.contains('active')) {
       navigation.classList.remove('active', 'js-slide-down');
       navigation.classList.add('js-slide-up');
+      isOpen = false;
     } else {
       navigation.classList.remove('js-slide-up');
       navigation.classList.add('active', 'js-slide-down');
+      isOpen = true;
+    }
+    if (window.innerWidth < 768 && isOpen === true) {
+      header.style.paddingBottom = '40px';
+    } else {
+      header.style.paddingBottom = '0px';
     }
   }.bind(navIcon);
   var toggleFixedHeader = function () {
     var parallax = document.querySelector('.parallax');
     var intro = document.querySelector('.intro');
     var parallaxHeight = parallax.offsetHeight;
+    var headerHeight = header.offsetHeight;
     if (document.body.scrollTop >= parallaxHeight) {
-      console.log(document.body.scrollTop, ' scrollTop');
       header.classList.remove('js-slide-up');
       header.classList.add('js-header--fixed', 'js-slide-down');
-      intro.style.paddingTop = '90px';
+      intro.style.paddingTop = headerHeight + 'px';
     } else {
       header.classList.remove('js-header--fixed', 'js-slide-down');
       header.classList.add('js-slide-up');
